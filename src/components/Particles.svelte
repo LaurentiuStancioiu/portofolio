e1189d85e6919fd3877861f4833110825eb22dff
Laurentiu Stancioiu <laurentiustancioiu@icloud.com>
2026-07-04 15:24:17 +0300

    feat: replace particles.js CDN with @tsparticles/svelte

---
+++ src/components/Particles.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Particles, { initParticlesEngine } from '@tsparticles/svelte';
+	import { loadSlim } from '@tsparticles/slim';
+	import { particlesOptions } from '$lib/particles';
+
+	let ready = $state(false);
+
+	onMount(() => {
+		initParticlesEngine(async (engine) => {
+			await loadSlim(engine);
+		}).then(() => {
+			ready = true;
+		});
+	});
+</script>
+
+{#if ready}
+	<Particles
+		id="tsparticles"
+		options={particlesOptions}
+		class="absolute inset-0 w-full h-full"
+	/>
+{/if}
